fd00a657601c2d32328f6108f90b92ac97266339
ue88114 <khoi.tran@sbb.ch>
2022-09-19 11:35:13 +0200

    solutions

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'css-init'
 import './style.css'
 import * as d3 from 'd3';
 import allCantons from '../data/ch-cantons.json';
+import allLakes from '../data/ch-lakes.json';
 import { ExtendedFeature, ExtendedFeatureCollection } from 'd3';
 
 // geoMercator converts from WGS85 to 2d screen coordinates
@@ -9,21 +10,26 @@ const projection = d3.geoMercator();
 
 // geoPath can generate <path d="xxx"> commands
 const pathDrawer = d3.geoPath(projection)
-  .pointRadius(3); // circle size for "Point" geometries
+.pointRadius(3); // circle size for "Point" geometries
 
 // fit the FeatureCollection inside a rectangle of 640x480
 projection.fitSize([640, 480], allCantons as ExtendedFeatureCollection);
 
 // add new path elements using d3
 d3.select('g.layer-cantons') // select group element
-  .selectAll('path') // select all path elements
-  .data(allCantons.features) // set data on all path elements
-  .join('path') // append a <path> element per element of the array
-  .attr('d', feature => pathDrawer(feature as ExtendedFeature)) // set the 'd' attribute of the path element using
-  .attr('fill', (_, i) => d3.schemeCategory10[i % d3.schemeCategory10.length]); // fill with a color
+.selectAll('path') // select all path elements
+.data(allCantons.features) // set data on all path elements
+.join('path') // append a <path> element per element of the array
+.attr('d', feature => pathDrawer(feature as ExtendedFeature)) // set the 'd' attribute of the path element using
+.attr('fill', (_, i) => d3.schemeCategory10[i % d3.schemeCategory10.length]); // fill with a color
 
 // Exercise 1: draw all lakes from ../data/ch-lakes.json
 // hint: make a new layer in index.html
+d3.select('g.layer-lakes')
+.selectAll('path')
+.data(allLakes.features)
+.join('path')
+.attr('d', feature => pathDrawer(feature as ExtendedFeature));
 
 // Exercise 1a: click / hover on lake will display its name to the <aside> element
 // hint: use .on('click', (event, feature) => d3.select('aside').text('hello world'))
@@ -42,11 +48,31 @@ const truesselStrasse2 = {
   }
 }
 
+d3.select('g.layer-other')
+.selectAll('path')
+.data([truesselStrasse2])
+.join('path')
+.attr('d', feature => pathDrawer(feature as ExtendedFeature));
+
 // Exercise 2a: draw a symbol instead using d3.symbol()
-const triangleSymbolDrawer = d3.symbol().type(d3.symbolTriangle).size(5);
+const triangleSymbolDrawer = d3.symbol().type(d3.symbolTriangle).size(100);
+
+d3.select('g.layer-symbols')
+.selectAll('path')
+.data([truesselStrasse2])
+.join('path')
+.attr('transform', feature => `translate(${projection((feature.geometry).coordinates as [number, number])})`)
+.attr('d', () => triangleSymbolDrawer());
+
 
 // Exercise 3: add labels to cantons
 // hint: use path pathDrawer.centroid() to find the center of a GeoJSON Feature
+d3.select('g.layer-canton-labels')
+.selectAll('text')
+.data(allCantons.features)
+.join('text')
+.attr('transform', feature => `translate(${pathDrawer.centroid(feature as ExtendedFeature)})`)
+.text(feature => feature.properties.name);
 
 // Exercise 4: draw the path in ../data/path-from-bern-to-thun.json
 
